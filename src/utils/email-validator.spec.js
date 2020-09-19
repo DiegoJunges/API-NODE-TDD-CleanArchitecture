f1f0413b00c609fb5f19bf2207815b24018f68f2
Diego Junges <diegojunges@hotmail.com>
2020-09-19 19:56:31 -0300

    moved email validator to its own file

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,22 +1,27 @@
+const EmailValidator = require('./email-validator')
 const validator = require('validator')
 
-class EmailValidator {
-  isValid(email) {
-    return validator.isEmail(email)
-  }
+const makeSut = () => {
+  return new EmailValidator()
 }
 
 describe('Email Validator', () => {
   it('Should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@email.com')
     expect(isEmailValid).toBe(true)
   })
 
   it('Should return false if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  it('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@email.com')
+    expect(validator.email).toBe('any_email@email.com')
   })
 })
