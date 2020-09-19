dd393082de2637d518d55fb0d0ea0c1603ddefa5
Diego Junges <diegojunges@hotmail.com>
2020-09-19 20:30:11 -0300

    another fix on moved errors

--- src/utils/errors/index.js
+++ src/utils/errors/index.js
@@ -1,5 +1,5 @@
-const MissingParamError = require('../errors/missing-param-error')
-const InvalidParamError = require('../errors/invalid-param-error')
+const MissingParamError = require('./missing-param-error')
+const InvalidParamError = require('./invalid-param-error')
 
 module.exports = {
   MissingParamError,
